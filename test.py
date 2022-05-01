82cb62836861f0f621193537ce12add9f3ea49b8
nmxkik <fedorets_m@outlook.com>
2022-05-01 21:09:46 +0300

    Add tests

--- test.py
+++ test.py
@@ -1,18 +1,27 @@
+from turtle import clear
 import unittest
+from unittest.mock import patch, Mock
+
+from defer import return_value
 from calc import Calc
+from calculator import main
 
 c = Calc()
 
 
 class CalculateTests(unittest.TestCase):
 
-    def test_add_method(self):
+    def test_many_signs(self):
         result = c.calculate("(2+3)-(4*5)")
         self.assertEqual(result, -15)
 
-    def test_add_method_wrong_value(self):
+    def test_many_signs_wrong_value(self):
         result = c.calculate("2+2")
         self.assertNotEqual(result, 5)
+
+    def test_no_ints(self):
+        result = c.calculate("2.0 + 5.0")
+        self.assertEqual(result, 7.0)
 
     def test_add_method_invalid_value(self):
         self.assertRaises(
@@ -28,6 +37,13 @@ class CalculateTests(unittest.TestCase):
 
     def test_div_method_zero(self):
         self.assertRaises(ZeroDivisionError, c.calculate, "5/0")
+
+
+class MainLoopTest(unittest.TestCase):
+
+    @patch("builtins.input", return_value="exit")
+    def test_input(self, input):
+        self.assertRaises(SystemExit, main)
 
 
 if __name__ == '__main__':
